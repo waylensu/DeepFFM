829613f64b32e94c5ff27effc098d4c69fe034eb
waylensu <waylensu@163.com>
2017-06-26 10:18:47 +0800

    add sklearn auc

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 train.py --num_epochs=100 --lr=0.01 --data_dir=data --log_dir=save/deepffm_v0 --batch_size=1000 --num_workers=2 
+python3 train.py --num_epochs=100000 --data_dir=data --log_dir=save/deepffm_v0 --batch_size=1000 --num_workers=10 
--- tools/worker.py
+++ tools/worker.py
@@ -11,6 +11,7 @@ import os.path
 import time
 import logging
 import signal
+import sklearn.metrics import roc_auc_score
 
 import _init_paths
 from deepffm.data_reader import inputs
@@ -20,6 +21,7 @@ from deepffm.model import DeepFFM
 FLAGS = None
 
 def train(server):
+    logging.info('start train')
     # set train parameter
     batch_size = FLAGS.batch_size
 
@@ -37,11 +39,11 @@ def train(server):
         test_inds, test_vals, test_labels = inputs(test_file, FLAGS.batch_size, FLAGS.num_epochs)    
         with tf.variable_scope("model"):
             deepffm = DeepFFM(field_range, embed_size=8, l2_reg_lambda = 0.00001, NUM_CLASSES=2, inds=train_inds, vals=train_vals, labels=train_labels, linear=False)
-            global_step = tf.Variable(0, name='global_step', trainable=False)
+            global_step_op = tf.Variable(0, name='global_step', trainable=False)
 
-    lr = tf.train.exponential_decay(FLAGS.lr, global_step, 100, 0.98, staircase = True)
+    lr = tf.train.exponential_decay(FLAGS.lr, global_step_op, 100, 0.98, staircase = True)
     optimizer = tf.train.AdamOptimizer(lr)
-    train_op = optimizer.minimize(deepffm.loss, global_step = global_step)
+    train_op = optimizer.minimize(deepffm.loss, global_step = global_step_op)
 
     init_all_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
     merged = tf.summary.merge_all()
@@ -69,24 +71,27 @@ def train(server):
                              init_fn=init_fn,
                              ready_op=tf.report_uninitialized_variables(variables_to_save),
                              summary_writer=train_writer,
-                             global_step=global_step,
+                             global_step=global_step_op,
                              )
 
     with sv.managed_session(server.target, config=config) as sess, sess.as_default():
-        global_step_ = sess.run(global_step)
+        global_step = sess.run(global_step_op)
         sess.run(init_all_op)
-        step = 0
-        while not sv.should_stop() :
-            try:
+        try:
+            step = 0
+            while not sv.should_stop() :
                 # Train data
                 if step % 10 == 0:
                     start_time = time.time()
-                    _, summary, loss_value, accuracy, auc, lr_value = sess.run([train_op, merged, deepffm.loss, deepffm.accuracy, deepffm.auc, lr])
+                    _, summary, loss_value, accuracy, auc, lr_value, global_step, logits, labels = sess.run([train_op, merged, deepffm.loss, deepffm.accuracy, deepffm.auc, lr, global_step_op, deepffm.logits, deepffm.labels_])
+                    sklearn_auc = roc_auc_score(labels, logits)
                     duration = time.time() - start_time
-                    logging.info('Step %d: loss = %.5f, accuracy = %.5f, auc = %.5f, lr = %.5f.(%.5f sec)' % (step, loss_value, accuracy, auc, lr_value, duration))
+
+                    logging.info('Step %d, Global %d, : loss = %.5f, accuracy = %.5f, auc = %.5f, sk_auc = %.5f, lr = %.5f.(%.5f sec)' % (step, global_step, loss_value, accuracy, auc, sklearn_auc, lr_value, duration))
+
                 else:
                     _, summary = sess.run([train_op, merged])
-                train_writer.add_summary(summary, global_step_)
+                train_writer.add_summary(summary, global_step)
 
                 # Test data
                 if step % 100 == 0:
@@ -94,17 +99,22 @@ def train(server):
                     start_time = time.time()
                     summary, loss_value, accuracy, auc = sess.run([merged, deepffm.loss, deepffm.accuracy, deepffm.auc])
                     duration = time.time() - start_time
-                    logging.info('\tTest Step %d: loss = %.5f, accuracy = %.5f, auc = %.5f. (%.5f sec)' % (step, loss_value, accuracy, auc, duration))
-                    test_writer.add_summary(summary, global_step_)
+
+                    logging.info('\tTest Step %d, Global %d, : loss = %.5f, accuracy = %.5f, auc = %.5f. (%.5f sec)' % (step, global_step, loss_value, accuracy, auc, duration))
+
+                    test_writer.add_summary(summary, global_step)
                     deepffm.inds, deepffm.vals, deepffm.labels = [train_inds, train_vals, train_labels]
 
-            except tf.errors.OutOfRangeError:
-                logging.info('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
-            finally:
-                sv.stop()
-                logging.info('reached %s steps. worker stopped.', global_step_)
-                train_writer.close()
-                test_writer.close()
+                #logging.info('sv should stop: {}'.format(sv.should_stop()))
+                step += 1
+
+        except tf.errors.OutOfRangeError:
+            logging.info('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
+        finally:
+            sv.stop()
+            logging.info('reached %s steps. worker stopped.', global_step)
+            train_writer.close()
+            test_writer.close()
 
 def cluster_spec(num_workers, num_ps):
     """ More tensorflow setup for data parallelism """
@@ -168,7 +178,4 @@ if __name__ == '__main__':
     parser.add_argument('--task_index', default=0, type=int, help='Task index')
     parser.add_argument('--num_workers', default=1, type=int, help='Number of workers')
     FLAGS = parser.parse_args()
-    #FLAGS, unparsed = parser.parse_args()
-    #tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
     tf.app.run(main=main, argv=[sys.argv[0]])
-    #tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
